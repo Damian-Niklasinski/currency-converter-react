bde195120e00f929acd784ffd36a824b05b0c237
Damian Niklasinski <damianiklasinski@wp.pl>
2024-02-23 15:25:46 +0100

    change background size to cover and added min height.

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -3,20 +3,21 @@ import "./style.css";
 
 export const Clock = () => {
 
-    const [date, setDate] = useState((new Date()));
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         const intervalID = setInterval(() => {
             setDate(new Date());
         }, 1000);
 
-        return (() => {
+        return () => {
             clearInterval(intervalID);
         }
-        );
 
     });
+
     
+
     return (
         <>
             <p className="form__date">
